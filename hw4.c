196882a18e6e9137e64f0ef7cfb635d0762c3f6b
spacewhichusernicknamedecided <90033995+spacewhichusernicknamedecided@users.noreply.github.com>
2021-10-17 00:31:02 +0900

    Add files via upload

---
+++ hw4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int solve(int num)
+{
+	if (num < 2)
+	{
+		if (num % 2 == 0)
+			printf("0");
+		else printf("1");
+		return;
+	}
+	else
+	{
+		solve(num/2);
+		if (num % 2 == 0)
+			printf("0");
+		else printf("1");
+	}
+}
+
+int main(void)
+{
+	int num=20;
+	printf("please enter a number : %d\n", num);
+	solve(num);
+	return 0;
+}
