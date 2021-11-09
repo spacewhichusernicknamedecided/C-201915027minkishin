0113c1ef0dc862fd4b71f787638bf703100d9598
spacewhichusernicknamedecided <90033995+spacewhichusernicknamedecided@users.noreply.github.com>
2021-11-09 19:39:00 +0900

    Add files via upload

---
+++ hw5.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int line[5];
+	int i;
+
+	printf("5개의 정수를 입력하세요 : ");
+
+	for (i = 0; i < 5; i++)
+	{
+		scanf_s("%d", &line[i]);
+	}
+
+	printf("짝수 :");
+	for (i = 0; i < 5;i++)
+	{
+		if (line[i] % 2 == 0)
+		printf("%2d", line[i]);
+	}
+	printf("\n");
+	printf("홀수 :");
+	for (i = 0; i < 5;i++)
+	{
+		if (line[i] % 2 == 1)
+			printf("%2d", line[i]);
+	}
+
+	return 0;
+}
